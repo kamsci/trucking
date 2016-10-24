3cce0447d724a0ae5c0994462c79ab8f1b91d022
Krista Calderon <kamsci@gmail.com>
2016-10-24 12:09:07 -0700

    updated db types for referenceId and arrivalTime to String and Date, added first job into db

--- models/jobs.js
+++ models/jobs.js
@@ -4,12 +4,12 @@ var mongooseUniqueValidator = requpre('mongoose-unique-validator');
 
 var jobSchema = new Schema({
     title: { type: String, required: true },
-    referenceId: { type: Number, required: true, unique: true },
+    referenceId: { type: String, required: true, unique: true },
     jobComplete: { type: Boolean },
     stops: [{
         stopType: { type: String },
         cargoDescription: { type: String },
-        arrivalTime: { type: String },
+        arrivalTime: { type: Date },
         picture: { type: String },
         ladingType: { type: String },
         ladingComplete: { type: Boolean },
